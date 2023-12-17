e8c6513062c70729945f4d12f31a3daf4456974a
Artem Babak <dev@tararoutray.com>
2023-12-18 02:18:55 +0300

    another init

--- utils.py
+++ utils.py
@@ -40,5 +40,5 @@ def weights_init(m):
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
