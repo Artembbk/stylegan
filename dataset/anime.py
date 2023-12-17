a6095a6eca0d1e4264a114cc9ef8eb122c687905
Artem Babak <dev@tararoutray.com>
2023-12-18 00:07:42 +0300

    fix normalize

--- dataset/anime.py
+++ dataset/anime.py
@@ -18,10 +18,9 @@ class AnimeFacesDataset(Dataset):
         with open(index_file, 'r') as file:
             self.img_labels = json.load(file)
 
-        self.transform = transforms.Compose(
-            [
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-            ])
+    def normalize_negative_one(self, img):
+        normalized_input = (img - torch.amin(img)) / (torch.amax(img) - torch.amin(img))
+        return 2*normalized_input - 1
 
     def __len__(self):
         return len(self.img_labels)
@@ -30,8 +29,6 @@ class AnimeFacesDataset(Dataset):
         img_path = os.path.join(self.data_path, self.img_labels[idx])
         image = read_image(img_path)
         image = torch.tensor(image, dtype=torch.float32)
-        print(image.min(), image.max())
-        image = self.transform(image)
-        print(image.min(), image.max())
+        image = self.normalize_negative_one(image)
         return {"images": image}
 
