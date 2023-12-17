4f952be290d9d88a205aa6859db035a0ea9caa08
Artem Babak <dev@tararoutray.com>
2023-12-18 00:25:55 +0300

    aaaaaaaaaaa

--- normalize.py
+++ normalize.py
@@ -4,7 +4,7 @@ def normalize_negative_one(img):
     normalized_input = (img - torch.amin(img)) / (torch.amax(img) - torch.amin(img))
     return 2*normalized_input - 1
 
-def denormalize_from_negative_one(self, normalized_img):
+def denormalize_from_negative_one(normalized_img):
     denormalized_input = 0.5 * (normalized_img + 1.0)
     denormalized_input = denormalized_input * 255.0
     return denormalized_input.type(torch.uint8)
