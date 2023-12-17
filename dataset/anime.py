02f2ae7c9b22ed938cf339c4d890c685dea24d01
Artem Babak <dev@tararoutray.com>
2023-12-17 04:50:00 +0300

    fdfsf

--- dataset/anime.py
+++ dataset/anime.py
@@ -28,10 +28,10 @@ class AnimeFacesDataset(Dataset):
     
     def __getitem__(self, idx):
         img_path = os.path.join(self.data_path, self.img_labels[idx])
-        
-        if not os.path.exists(img_path):
-            print(self.img_labels[idx])
-        image = read_image(img_path)
+        try:
+            image = read_image(img_path)
+        except Exception as e:
+            print(img_path)
         image = torch.tensor(image, dtype=torch.float32)
         image = self.transform(image)
         return image
